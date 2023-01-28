bc693c8055c5970860bbce6de8dcc3e7c240a1d5
Nicolás A. Niño <58050431+Ste1nb0cK@users.noreply.github.com>
2023-01-28 15:33:13 -0500

    Modified some printing and the shell commands

--- MountingHDD/getbig.py
+++ MountingHDD/getbig.py
@@ -1,6 +1,6 @@
-import os
+import subprocess
 import sys
-
+import os
 #info to identify the disk
 aux = "Disk model: "
 disk_model = "TOSHIBA MQ04ABF1"
@@ -9,10 +9,14 @@ target = aux + disk_model
 mount_direction = '/SanJuanWolf'
 #Check that the mounting directory exists, create it in case it doesn't
 if not os.path.exists(mount_direction):
-    os.system('mkdir {}'.format(mount_direction))
+    print("The mounting direction does not exists, creating it.")
+    subprocess.run(["sudo", "mkdir", mount_direction], shell=False)
+    print("Mounting direction created at {}".format(mount_direction))
+
 else:
-    print("The mounting direction exists. Proceeding to mounting.")
+    print("The mounting direction exists.")
 #identification of the device
+print("Initiating Identification.")
 input = sys.stdin.read()
 cut_index = input.find(target)
 cutted_input = input[cut_index::]
@@ -21,5 +25,7 @@ candidate_line = splitted[8]
 if (candidate_line.endswith('Linux filesystem')): #check that the line is valid
     device = candidate_line.split()[0]
 #mount the disk
-os.system('sudo mount {} {}'.format(device, mount_direction))
-print("Ya no estoy chikito")
+print("Mounting")
+subprocess.run(["sudo", "mount", device, mount_direction], shell=False)
+print("Mounted at {}".format(mount_direction))
+print("------------------------------Ya no estoy chikito------------------------------")
